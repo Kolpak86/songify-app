ee4390805c62b84006c2d63fef04e34fb82e7871
Andrew Blotski <ablotski@twiced.de>
2022-01-21 14:05:22 +0300

    material module

--- apps/songify/src/app/app.module.ts
+++ apps/songify/src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular-production/material';
+import { CoreDataModule } from '@angular-production/core-data';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    CoreDataModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
